c91925c5605e61ff0b77035ca1e9cc8cf256bede
sampath reddy <sampathreddy2127@gmail.com>
2020-12-27 20:52:27 -0600

    refactoring to better enum usage

--- src/patterns/factory/VehicleFactory.java
+++ src/patterns/factory/VehicleFactory.java
@@ -4,14 +4,6 @@ public class VehicleFactory {
 
     public Vehicle getVehicle( VehicleType vehicleType ) {
 
-        if ( vehicleType.equals (VehicleType.CAR) ) {
-            return new Car ();
-        } else if ( vehicleType.equals(VehicleType.BUS) ) {
-            return new Bus ();
-        } else if ( vehicleType.equals (VehicleType.ELECTRIC_CAR) ) {
-            return new ElectricCar ();
-        } else {
-            return null;
-        }
+        return vehicleType.getVehicle ();
     }
 }
--- src/patterns/factory/VehicleType.java
+++ src/patterns/factory/VehicleType.java
@@ -2,5 +2,22 @@ package patterns.factory;
 
 public enum VehicleType {
 
-    CAR,BUS,ELECTRIC_CAR;
+    CAR{
+        public Vehicle getVehicle() {
+           return new Car ();
+        }
+    } ,
+    BUS{
+        public Vehicle getVehicle() {
+            return new Bus ();
+        }
+    }
+    ,ELECTRIC_CAR {
+        public Vehicle getVehicle() {
+            return new ElectricCar ();
+        }
+    };
+
+
+    abstract Vehicle getVehicle();
 }
